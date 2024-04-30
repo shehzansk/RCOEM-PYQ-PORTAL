3d9e5a610cd86360c1a98352ee9c4a4dcf213df6
Shehzan Sheikh <sshahzan0@gmail.com>
2024-04-30 21:16:50 +0530

    Added new Subjects
    SE, DM, CLE

--- src/portal.js
+++ src/portal.js
@@ -3,29 +3,23 @@ import "./App.css"
 import downloadPng from './components/downloadPng.png'
 
 function Portal() {
-  const [degree, setDegree] = useState('');
   const [branch, setBranch] = useState('');
   const [subject, setSubject] = useState('');
   const [showLink, setShowLink] = useState(false);
 
   const branchData = {
     "be": {
-      "cse": ["Operating System", "Design and Analysis", "Software Engineering"],
-      // I have to add all remaining subjects here !!!
+      "cse": ["Operating System", "Design and Analysis", "Software Engineering", "Discrete Mathematics", "Cyber Law & Ethics"],
+      //I have to add all remaining subjects here !!!
     }
   };
 
   const subjectDriveLinks = {
     "Operating System": "https://drive.google.com/drive/folders/18r9armnB3Kx_2RL-Ctr3F_aE-mdyQoMf?usp=sharing",
     "Design and Analysis": "https://drive.google.com/drive/folders/1xJY18YOsYdg5ZFTXFPE_rgXFCZiGYHhd?usp=sharing",
-    "Software Engineering": "https://drive.google.com/your_SE_link",
-  };
-
-  const handleDegreeChange = (event) => {
-    setDegree(event.target.value);
-    setBranch('');
-    setSubject('');
-    setShowLink(false); 
+    "Software Engineering": "https://drive.google.com/drive/folders/1r3QbMuvY88QC8s_6Rk5MtilW64cW2zA6?usp=sharing",
+    "Discrete Mathematics": "https://drive.google.com/drive/folders/1pJwI2BBqm4RGQ7AJcn-60-itpYjgcp5n?usp=sharing",
+    "Cyber Law & Ethics": "https://drive.google.com/drive/folders/1ExYPUdZ5X-dc5npGzXsPag06piXytnDo?usp=sharing",
   };
 
   const handleBranchChange = (event) => {
@@ -45,30 +39,24 @@ function Portal() {
   
   return (
     <div id='selectBox'>
-      <select value={degree} onChange={handleDegreeChange}>
-        <option value="">Select Degree</option>
-        <option value="be">B.E.</option>
-      </select>
-      <br/>
       <select value={branch} onChange={handleBranchChange}>
         <option value="">Select Branch</option>
-        {degree && branchData[degree] &&
-          Object.keys(branchData[degree]).map((key) => (
-            <option key={key} value={key}> {key.toUpperCase()} </option>
-          ))}
+        {Object.keys(branchData["be"]).map((key) => (
+          <option key={key} value={key}> {key.toUpperCase()} </option>
+        ))}
       </select>
       <br/>
       <select value={subject} onChange={handleSubjectChange}>
         <option value="">Select Subject</option>
-        {branch && branchData[degree][branch] &&
-          branchData[degree][branch].map((subj) => (
+        {branch && branchData["be"][branch] &&
+          branchData["be"][branch].map((subj) => (
             <option key={subj} value={subj}> {subj} </option>
           ))} 
       </select>
       <br/>
       <button onClick={handleShowLink}>Show PYQs</button>
 
-      {showLink && branch !== "" && degree !== "" && subject !== "" && (
+      {showLink && branch !== "" && subject !== "" && (
         <div>
           <br />
           <button >

cfc538b21b9ce9246ef0303005c83cbbf7a8139f
Shehzan Sheikh <sshahzan0@gmail.com>
2024-05-18 16:59:59 +0530

    Added Subjects

--- src/portal.js
+++ src/portal.js
@@ -9,7 +9,22 @@ function Portal() {
 
   const branchData = {
     "be": {
-      "cse": ["Operating System", "Design and Analysis", "Software Engineering", "Discrete Mathematics", "Cyber Law & Ethics"],
+      "cse": [
+        "Artificial Intelligence",
+        "Business Communication",
+        "Compiler Design",
+        "Computer Architecture and Organization",
+        "Cyber Law & Ethics",
+        "Data Structures",
+        "Database Management System",
+        "Design and Analysis",
+        "Digital Electronics",
+        "Discrete Mathematics",
+        "Object Oriented Programming",
+        "Operating System",
+        "Software Engineering",
+        "Theory of Computation"
+      ],
       //I have to add all remaining subjects here !!!
     }
   };
@@ -20,6 +35,16 @@ function Portal() {
     "Software Engineering": "https://drive.google.com/drive/folders/1r3QbMuvY88QC8s_6Rk5MtilW64cW2zA6?usp=sharing",
     "Discrete Mathematics": "https://drive.google.com/drive/folders/1pJwI2BBqm4RGQ7AJcn-60-itpYjgcp5n?usp=sharing",
     "Cyber Law & Ethics": "https://drive.google.com/drive/folders/1ExYPUdZ5X-dc5npGzXsPag06piXytnDo?usp=sharing",
+    "Artificial Intelligence": "https://drive.google.com/drive/folders/1TwQmIv_ulSdIc0jbRvjzkJ3r__9qZWWF?usp=drive_link",
+    "Business Communication": "https://drive.google.com/drive/folders/1QsGJgUXAP_68oZrKtQpXwZtIef0KlRnb?usp=drive_link",
+    "Compiler Design": "https://drive.google.com/drive/folders/1SPQxpKVDBnMEcQhrg_pbR7FWKagur_d7?usp=drive_link",
+    "Computer Architecture and Organization": "https://drive.google.com/drive/folders/177RwyW3w4bTk4aw-HBJJvNOG8WVCy176?usp=drive_link",
+    "Data Structures": "https://drive.google.com/drive/folders/1AS6-binrdZGhysz9AlNZ5-WNJVYaYIne?usp=drive_link",
+    "Database Management System": "https://drive.google.com/drive/folders/1PpE08o6lwJvAsNS_uoUsjDAvdl44LrA_?usp=drive_link",
+    "Digital Electronics": "https://drive.google.com/drive/folders/1s1TR9jgfzhk19e0Ofbyov5TSkENikVl-?usp=drive_link",
+    "Object Oriented Programming": "https://drive.google.com/drive/folders/1GvNoom90zHL5aT1UySVqW9YLCfFD5Kff?usp=drive_link",
+    "Theory of Computation": "https://drive.google.com/drive/folders/13879AnxLFpM6M9t8eflbKNwA4oD8TZ2V?usp=drive_link",
+
   };
 
   const handleBranchChange = (event) => {
@@ -59,11 +84,13 @@ function Portal() {
       {showLink && branch !== "" && subject !== "" && (
         <div>
           <br />
-          <button >
+          
           <a href={subjectDriveLinks[subject]} target="_blank" rel="noopener noreferrer" id='downloadLink'>
-              Click Here To Download <img src={downloadPng} id='downImg'></img>
+              <button >
+                Click Here To Download <img src={downloadPng} alt="Download" id='downImg'></img>
+              </button>
           </a>
-          </button>
+          
         </div>
       )}
 
